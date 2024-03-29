a1c7784495a1a4e642f6cc45e21598cfbeeea20f
Oliver Tosky <olivertosky@gmail.com>
2024-03-29 00:03:51 -0400

    first pass at flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+      defaultPackage = {
+        x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+        aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
+      };
+
+      homeConfigurations = {
+        "olivertosky@ot-framework" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [ 
+            ./home/users/olivertosky/home.nix 
+          ];
+        };
+      };
+  };
+}
---
+++ home/users/olivertosky/home.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.stateVersion = "22.11";
+  home.username = "olivertosky";
+  home.homeDirectory = "/home/olivertosky";
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  home.packages = with pkgs; [
+    nixpkgs-fmt
+  ];
+
+  imports = [
+    ../../apps/nvim
+  ];
+
+}
