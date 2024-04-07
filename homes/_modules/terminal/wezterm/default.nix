538b5497c6855964d73ddac270769ca7e5d46287
Oliver Tosky <olivertosky@gmail.com>
2024-04-07 19:55:36 -0400

    try specifying unstable wezterm

--- homes/_modules/terminal/wezterm/default.nix
+++ homes/_modules/terminal/wezterm/default.nix
@@ -1,7 +1,12 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   config = {
     programs.wezterm = {
       enable = true;
+      package = pkgs.unstable.wezterm;
       extraConfig = builtins.readFile ./wezterm.lua;
     };
   };
