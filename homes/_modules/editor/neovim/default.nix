91d6fdbdda1ea609e91ae0ab7960baeb57eda974
Oliver Tosky <olivertosky@gmail.com>
2024-04-07 19:22:04 -0400

    enable neovim?

--- homes/_modules/editor/neovim/default.nix
+++ homes/_modules/editor/neovim/default.nix
@@ -3,54 +3,36 @@
   pkgs,
   lib,
   ...
-}:
-with lib; let
-  cfg = config.lazyvim;
-in {
-  options.lazyvim = {
-    enable = mkOption {
-      default = true;
-      description = "Enable lazyvim";
-      type = types.bool;
-    };
-
-    userConfig = mkOption {
-      default = null;
-      description = "lazyvim extra config";
-      type = with types; nullOr path;
-    };
+}: {
+  programs.neovim = {
+    enable = true;
   };
-  config = mkIf (cfg.enable) {
-    home = {
-      file = {
-        ".config/nvim" = {
-          recursive = true;
-          source = ./nvim;
-        };
-        ".config/nvim/lua/custom" = mkIf (cfg.userConfig != null) {
-          recursive = true;
-          source = cfg.userConfig;
-        };
+
+  home = {
+    file = {
+      ".config/nvim" = {
+        recursive = true;
+        source = ./nvim;
       };
-      packages = with pkgs; [
-        # telescope dependency
-        ripgrep
-        # build language servers
-        nodejs
-        python3
-        cargo
-        lua51Packages.jsregexp
-        unzip
-        # for git ui
-        lazygit
-
-        # lsps
-        nil
-
-        # formatters
-        alejandra
-        shfmt
-      ];
     };
+    packages = with pkgs; [
+      # telescope dependency
+      ripgrep
+      # build language servers
+      nodejs
+      python3
+      cargo
+      lua51Packages.jsregexp
+      unzip
+      # for git ui
+      lazygit
+
+      # lsps
+      nil
+
+      # formatters
+      alejandra
+      shfmt
+    ];
   };
 }
