8035add1ea6c87652e43e9f927dbcbce47e4b46b
Oliver Tosky <olivertosky@gmail.com>
2024-04-14 19:15:29 -0400

    fix font module

--- homes/_modules/desktop/common/font.nix
+++ homes/_modules/desktop/common/font.nix
@@ -3,7 +3,7 @@
     enable = true;
     monospace = {
       family = "JetBrains Mono Nerd Font";
-      package = pkgs.nerdfonts.override {fonts = ["JetBrains Mono"];};
+      package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
     };
     regular = {
       family = "JetBrains Mono";
