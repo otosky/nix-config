121bf04d1804e47faa8e46f68379f80c01d6e42f
Oliver Tosky <olivertosky@gmail.com>
2024-04-14 18:51:53 -0400

    add waybar css

--- homes/_modules/desktop/common/wayland-wm/waybar.nix
+++ homes/_modules/desktop/common/wayland-wm/waybar.nix
@@ -82,5 +82,192 @@ in {
         ];
       };
     };
+    style = ''
+      * {
+        border: none;
+        font-family: JetBrains Mono, "Font Awesome 5 Free";
+        font-size: 12px;
+        font-weight: bold;
+        min-height: 0;
+      }
+
+      window#waybar {
+        background-color: transparent;
+      }
+
+      window > box {
+        background: rgba(26, 27, 38, 1);
+        border-radius: 10px;
+        min-width: 46px;
+        margin: 10px;
+        background-clip: border-box;
+        box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.25), 0 1px 3px 1px rgba(0, 0, 0, 0.25),
+          0 2px 6px 2px rgba(0, 0, 0, 0.25);
+      }
+
+      #taskbar {
+        background: #1a1b26;
+        padding: 0 0.6em;
+        margin-right: 10px;
+        margin-left: 4px;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        border-radius: 10px;
+        color: #ebdbb2;
+      }
+
+      #taskbar button {
+        border-radius: 10px;
+        background: #24283b;
+        color: #a89984;
+        margin-right: 6px;
+      }
+
+      #taskbar button.minimized {
+        border-radius: 10px;
+        background: #24283b;
+        color: #a89984;
+      }
+
+      #taskbar button.active {
+        border-radius: 10px;
+        background: #4e635b;
+        color: #ebdbb2;
+      }
+
+      #disk {
+        color: #89ddff;
+      }
+
+      #memory {
+        color: #c792ea;
+      }
+
+      #cpu {
+        color: #ffcb6b;
+      }
+
+      #battery {
+        color: #c3e88d;
+      }
+
+      #temperature {
+        color: #c792ea;
+      }
+
+      #temperature {
+        color: #c792ea;
+      }
+
+      #temperature.critical {
+        background-color: #f07178;
+        color: #1a1b26;
+      }
+
+      #pulseaudio {
+        color: #ffcb6b;
+      }
+
+      #pulseaudio.muted {
+        background-color: #f07178;
+      }
+
+      @keyframes gradient {
+        0% {
+          background-position: 0% 50%;
+        }
+        50% {
+          background-position: 100% 50%;
+        }
+        100% {
+          background-position: 0% 50%;
+        }
+      }
+
+      #workspaces,
+      #clock,
+      #custom-power,
+      #window,
+      #disk,
+      #cpu,
+      #memory,
+      #battery,
+      #network,
+      #tray {
+        padding: 0 5px;
+        margin: 2px;
+      }
+
+      .modules-left,
+      .modules-center,
+      .modules-right {
+        background-color: #24283b;
+        margin: 5px 5px 5px 5px;
+        border-radius: 10px;
+        background-clip: padding-box;
+      }
+
+      #workspaces button {
+        padding: 0 4px;
+        min-width: 18px;
+        color: #82aaff;
+      }
+
+      #workspaces button:hover {
+        background-color: rgba(0, 0, 0, 0.2);
+      }
+
+      #workspaces button.active {
+        color: #c792ea;
+      }
+
+      #workspaces button.focused {
+        color: #c792ea;
+      }
+
+      #workspaces button.urgent {
+        color: #f07178;
+      }
+
+      #memory {
+        color: #c792ea;
+      }
+
+      #clock {
+        color: #82aaff;
+      }
+
+      #mode {
+        color: #ffcb6b;
+      }
+
+      #window {
+        color: #c3e88d;
+        /*color: #b0bec5;*/
+        background-color: transparent;
+      }
+
+      #custom-power {
+        color: #f25287;
+        background-color: #1a1b26;
+      }
+
+
+      @keyframes blink {
+        to {
+          background-color: #1a1b26;
+          color: #f07178;
+        }
+      }
+
+      #network {
+        color: #c792ea;
+      }
+
+      #network.disconnected {
+        background-color: #f07178;
+        color: #1a1b26;
+      }
+    '';
   };
 }
