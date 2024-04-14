d310c549bcc15b29e0f41f7db6ed7e791112370e
Oliver Tosky <olivertosky@gmail.com>
2024-04-13 21:04:04 -0400

    fixes on disko

--- luks-btrfs.nix
+++ luks-btrfs.nix
@@ -1,53 +1,51 @@
 {
-  disk = {
-    ssd = {
-      type = "disk";
-      device = "/dev/sda1";
-      content = {
-        type = "table";
-        format = "gpt";
-        partitions = {
-          ESP = {
-            priority = 1;
-            name = "ESP";
-            start = "1MiB";
-            end = "512MiB";
-            bootable = true;
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              options = [
-                "defaults"
-              ];
-            };
-          };
-
-          luks = {
-            end = "100%";
-            content = {
-              type = "luks";
-              name = "crypted";
-              settings = {
-                allowDiscards = true;
-              };
-              keyFile = "/tmp/secret.key";
+  disko.device = {
+    disk = {
+      ssd = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              name = "ESP";
+              size = "512M";
               content = {
-                type = "btrfs";
-                extraArgs = ["-f"];
-                subvolumes = {
-                  "/root" = {
-                    mountpoint = "/";
-                    mountOptions = ["compress=zstd"];
-                  };
-                  "/nix" = {
-                    mountOptions = ["compress=zstd" "noatime"];
-                  };
-                  "/persist" = {
-                    mountOptions = ["compress=zstd" "noatime"];
-                  };
-                  "/swap" = {
-                    mountOptions = ["noatime"];
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                settings = {
+                  allowDiscards = true;
+                };
+                keyFile = "/tmp/secret.key";
+                content = {
+                  type = "btrfs";
+                  extraArgs = ["-f"];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = ["compress=zstd"];
+                    };
+                    "/nix" = {
+                      mountOptions = ["compress=zstd" "noatime"];
+                    };
+                    "/persist" = {
+                      mountOptions = ["compress=zstd" "noatime"];
+                    };
+                    "/swap" = {
+                      mountOptions = ["noatime"];
+                    };
                   };
                 };
               };
