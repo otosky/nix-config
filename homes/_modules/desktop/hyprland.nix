126f96dc8cc3f56c88ab0ca1ac5fe1394baea10c
Oliver Tosky <olivertosky@gmail.com>
2024-04-17 00:27:19 -0400

    Revert "add wallpaper"
    This reverts commit 430da8156448b5c8ca685ebe50b6a0240809edbc.

--- homes/_modules/desktop/hyprland.nix
+++ homes/_modules/desktop/hyprland.nix
@@ -6,10 +6,6 @@
 }: let
   hyprland = pkgs.inputs.hyprland.hyprland.override {wrapRuntimeDeps = false;};
   xdph = pkgs.inputs.hyprland.xdg-desktop-portal-hyprland.override {inherit hyprland;};
-  wallpaper = pkgs.fetchurl {
-    url = "https://github.com/otosky/dotfiles/blob/main/dot_config/sway/milad-fakurian-JrMz6hVQeu4-unsplash.jpg";
-    hash = "sha256-D6L8ZzBdaknUq7lGbT5OUZC88JHmQg8eojaXpeI1n5E=";
-  };
 in {
   imports = [
     ./common
@@ -89,7 +85,6 @@ in {
           "layersOut,1,3,easeinback,slide"
         ];
       };
-      exec = ["${pkgs.swaybg}/bin/swaybg -i ${wallpaper} --mode fill"];
 
       bind =
         [
