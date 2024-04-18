d8d6019bf6750b462a19f7cacd3044f30c351b60
Oliver Tosky <olivertosky@gmail.com>
2024-04-17 21:13:41 -0400

    fix for gimp

--- homes/_modules/extra/default.nix
+++ homes/_modules/extra/default.nix
@@ -9,9 +9,22 @@
       _1password
       _1password-gui
       brave
-      gimp-with-plugins
       libreoffice
       zoom-us
+
+      https://github.com/montchr/dotfield/blob/78de8ff316ccb2d34fd98cd9bfd3bfb5ad775b0e/home/profiles/desktop/common.nix #L17
+      # HACK: The `gap` plugin is broken upstream, and I have no intent on using it anyway.
+      # FIXME: Remove the override when merged: <https://github.com/NixOS/nixpkgs/pull/295257>
+      (pkgs.gimp-with-plugins.override {
+        plugins = let
+          # <https://github.com/Scrumplex/nixpkgs/blob/cca25fd345f2c48de66ff0a950f4ec3f63e0420f/pkgs/applications/graphics/gimp/wrapper.nix#L5C1-L6C99>
+          allPlugins = lib.filter (pkg: lib.isDerivation pkg && !pkg.meta.broken or false) (
+            lib.attrValues pkgs.gimpPlugins
+          );
+          pred = pkg: pkg != pkgs.gimpPlugins.gimp && pkg != pkgs.gimpPlugins.gap;
+        in
+          lib.filter pred allPlugins;
+      })
     ];
   };
   services = {
