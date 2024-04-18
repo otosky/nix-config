226aa8fc622ef6970cd5e00940867ae17beeb98e
Oliver Tosky <olivertosky@gmail.com>
2024-04-17 20:46:02 -0400

    fix syntax

--- homes/_modules/extra/default.nix
+++ homes/_modules/extra/default.nix
@@ -4,12 +4,14 @@
   lib,
   ...
 }: {
-  home.packages = with pkgs; [
-    _1password
-    _1password-gui
-    brave
-    gimp-with-plugins
-    libreoffice
-    zoom-us
-  ];
+  home = {
+    packages = with pkgs; [
+      _1password
+      _1password-gui
+      brave
+      gimp-with-plugins
+      libreoffice
+      zoom-us
+    ];
+  };
 }
