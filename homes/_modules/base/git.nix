307381662e8e20e1441d59fa5ef7fb1d7baeb877
Oliver Tosky <olivertosky@gmail.com>
2024-04-21 14:31:23 -0400

    re-specify gpg program

--- homes/_modules/base/git.nix
+++ homes/_modules/base/git.nix
@@ -16,6 +16,7 @@ in {
 
       user.signing.key = "005F0191AFEEAD6FF90F78DB71E926104222B60F";
       commit.gpgSign = true;
+      gpg.program = "${config.programs.gpg.package}/bin/gpg2";
 
       merge.conflictStyle = "zdiff3";
       commit.verbose = true;
