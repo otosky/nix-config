11b3ef69527cc2b355c7eacea798bc972a5e5dde
Oliver Tosky <olivertosky@gmail.com>
2024-04-21 22:55:10 -0400

    add an installer iso flake
    closes #4

---
+++ installer/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Minimal NixOS installation media";
+  inputs = {
+    nixpkgs.url = "nixpkgs/23.11";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    disko,
+  }: {
+    nixosConfigurations = {
+      customIso = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          disko.nixosModules.disko
+
+          ({pkgs, ...}: {
+            environment.systemPackages = with pkgs; [
+              neovim
+              git
+              sops
+              gnupg
+            ];
+
+            boot.swraid.enable = nixpkgs.lib.mkForce false;
+          })
+        ];
+      };
+    };
+  };
+}
