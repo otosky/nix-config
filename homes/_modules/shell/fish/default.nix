b5a9bb23e3767275b7d3a84274c9493df898b85c
Oliver Tosky <olivertosky@gmail.com>
2024-07-28 10:57:01 -0400

    remove obsolete fish init

--- homes/_modules/shell/fish/default.nix
+++ homes/_modules/shell/fish/default.nix
@@ -32,15 +32,15 @@
           name = "fzf";
           src = pkgs.fishPlugins.fzf-fish.src;
         }
-        {
-          name = "tide";
-          src = pkgs.fishPlugins.tide.src;
-          # tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
-        }
         # {
-        #   name = "pure";
-        #   src = pkgs.fishPlugins.pure.src;
+        #   name = "tide";
+        #   src = pkgs.fishPlugins.tide.src;
+        #   # tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
         # }
+        {
+          name = "pure";
+          src = pkgs.fishPlugins.pure.src;
+        }
       ];
 
       shellInit = ''
@@ -52,26 +52,16 @@
         set -gx MISE_ENV_FILE .env
         set -gx devbox_no_prompt true
         set -gx EDITOR nvim
-        #tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
       '';
 
       interactiveShellInit = ''
         fish_add_path $HOME/.local/bin
-        #mise activate fish | source
-
         fish_add_path $HOME/bin/
+
         fish_add_path $HOME/.krew/bin
-        # fish_add_path (dirname (mise which cargo))
         fish_add_path $HOME/.local/share/coursier/bin
         fish_add_path $HOME/.ghcup/bin
         fish_add_path $HOME/.docker/bin # for macOS
-        if command -q opam
-            eval $(opam env)
-        end
-
-        fish_ssh_agent
-
-        # zoxide init fish | source
       '';
     };
   };
