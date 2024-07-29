f51486ef966c926e2615d285ba14775645f2edc1
Oliver Tosky <olivertosky@gmail.com>
2024-07-28 23:47:56 -0400

    use pinentry-curses for gpg

--- homes/_modules/base/gpg.nix
+++ homes/_modules/base/gpg.nix
@@ -11,7 +11,7 @@
       "414D4FC4828AAC81412AED6E9CC9DFD09CA07570"
     ];
     enableExtraSocket = true;
-    pinentryPackage = pkgs.pinentry-tty;
+    pinentryPackage = pkgs.pinentry-curses;
   };
 
   programs = let
