e9e534832d2e31a2d9e9d2c5398ca4f2bb3966cc
Oliver Tosky <olivertosky@gmail.com>
2024-09-27 18:36:50 -0400

    add desktop entry for sleek

--- homes/_modules/desktop/common/wayland-wm/default.nix
+++ homes/_modules/desktop/common/wayland-wm/default.nix
@@ -7,29 +7,41 @@
     ./swaync.nix
   ];
 
-  xdg.mimeApps.enable = true;
-
-  home.packages = with pkgs; [
-    grim
-    gtk3 # For gtk-launch
-    imv
-    mimeo
-    pulseaudio
-    slurp
-    waypipe
-    wf-recorder
-    wl-clipboard
-    wl-mirror
-    xdg-utils
-    ydotool
-    libnotify
-  ];
-
-  home.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = 1;
-    QT_QPA_PLATFORM = "wayland";
-    LIBSEAT_BACKEND = "logind";
+  home = {
+    packages = with pkgs; [
+      grim
+      gtk3 # For gtk-launch
+      imv
+      mimeo
+      pulseaudio
+      slurp
+      waypipe
+      wf-recorder
+      wl-clipboard
+      wl-mirror
+      xdg-utils
+      ydotool
+      libnotify
+    ];
+    sessionVariables = {
+      MOZ_ENABLE_WAYLAND = 1;
+      QT_QPA_PLATFORM = "wayland";
+      LIBSEAT_BACKEND = "logind";
+    };
   };
 
-  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-wlr];
+  xdg = {
+    mimeApps.enable = true;
+    portal.extraPortals = [pkgs.xdg-desktop-portal-wlr];
+    desktopEntries = {
+      sleek = {
+        name = "sleek";
+        exec = "/home/olivertosky/bin/sleek-2.0.14.AppImage";
+        terminal = false;
+        type = "Application";
+        comment = "todo.txt manager for Linux, Windows and MacOS, free and open-source (FOSS)";
+        categories = ["ProjectManagement"];
+      };
+    };
+  };
 }
