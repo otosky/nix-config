55ad14941b91d6291311707164c7e9d5f53b99e2
Oliver Tosky <olivertosky@gmail.com>
2024-10-20 16:00:20 -0400

    replace framework config

---
+++ hosts/ot-framework/default.nix
@@ -0,0 +1,64 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-pc-ssd
+    inputs.hardware.nixosModules.framework-13th-gen-intel
+
+    ./hardware-configuration.nix
+
+    ../common/base
+    ../common/users/olivertosky
+
+    ../common/optional/greetd.nix
+    ../common/optional/pipewire.nix
+    ../common/optional/quietboot.nix
+    ../common/optional/1password.nix
+  ];
+
+  networking = {
+    hostName = "ot-framework";
+    useDHCP = true;
+    networkmanager = {
+      enable = true;
+    };
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+    binfmt.emulatedSystems = [
+      "aarch64-linux"
+      "i686-linux"
+    ];
+    initrd = {
+      supportedFilesystems = ["nfs"];
+      kernelModules = ["nfs"];
+    };
+  };
+
+  services = {
+    fwupd.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    fw-ectool
+    powertop
+    lact
+  ];
+
+  programs = {
+    adb.enable = true;
+    dconf.enable = true;
+    kdeconnect.enable = true;
+  };
+
+  hardware = {
+    graphics.enable = true;
+    opentabletdriver.enable = true;
+  };
+
+  system.stateVersion = "23.11";
+}
---
+++ hosts/ot-framework/hardware-configuration.nix
@@ -0,0 +1,29 @@
+{
+  imports = [
+    ../common/optional/disko.nix
+  ];
+
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "ahci"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+      ];
+      kernelModules = ["kvm-intel"];
+    };
+    loader = {
+      systemd-boot = {
+        enable = true;
+        consoleMode = "max";
+      };
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  nixpkgs.hostPlatform.system = "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = true;
+}
