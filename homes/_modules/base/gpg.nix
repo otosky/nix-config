04f3a8c5c2824196aedf5de9e6bab29aceac2a98
Oliver Tosky <olivertosky@gmail.com>
2024-10-27 14:36:36 -0400

    fetch pub key from keybase
    closes #6

--- homes/_modules/base/gpg.nix
+++ homes/_modules/base/gpg.nix
@@ -22,6 +22,10 @@
       ''
         gpgconf --launch gpg-agent
       '';
+    gpgKey = pkgs.fetchurl {
+      url = "https://keybase.io/otosky/pgp_keys.asc";
+      sha256 = "Fm4o9XSVuJgcdcGS7d9z2c2lEbe0W++5uj0Iz9g+VcY=";
+    };
   in {
     # Start gpg-agent if it's not running or tunneled in
     # SSH does not start it automatically, so this is needed to avoid having to use a gpg command at startup
@@ -37,7 +41,7 @@
       };
       publicKeys = [
         {
-          source = ../../olivertosky/pgp.asc;
+          source = "${gpgKey}";
           trust = 5;
         }
       ];
