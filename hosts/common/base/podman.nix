3a6551adb3b997ad9c7639e8133198adc511cf33
Oliver Tosky <olivertosky@gmail.com>
2024-12-09 18:15:59 -0500

    add podman-compose

--- hosts/common/base/podman.nix
+++ hosts/common/base/podman.nix
@@ -1,10 +1,21 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
     dockerSocket.enable = true;
     defaultNetwork.settings.dns_enabled = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    #docker-compose # start group of containers for dev
+    podman-compose # start group of containers for dev
+  ];
 
   # TODO impermanence
   # environment.persistence = {
