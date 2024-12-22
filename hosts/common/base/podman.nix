7aac713c652af75fe5a66cbd220c8717df045626
Oliver Tosky <olivertosky@gmail.com>
2024-12-22 14:09:42 -0500

    add distrobox pkg

--- hosts/common/base/podman.nix
+++ hosts/common/base/podman.nix
@@ -11,10 +11,10 @@
   };
 
   environment.systemPackages = with pkgs; [
-    dive # look into docker image layers
-    podman-tui # status of containers in the terminal
-    #docker-compose # start group of containers for dev
-    podman-compose # start group of containers for dev
+    dive
+    podman-tui
+    podman-compose
+    distrobox
   ];
 
   # TODO impermanence
