99b80efd03125d1fc81c8f6a1a297578829fa828
Oliver Tosky <olivertosky@gmail.com>
2026-02-21 21:28:26 -0800

    fix: allow all systems

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
-    systems.url = "github:nix-systems/default-linux";
+    systems.url = "github:nix-systems/default";
 
     # Home manager
     home-manager = {
