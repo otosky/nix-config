83be65101d23f40161ca4bfec428f93131801904
Oliver Tosky <olivertosky@gmail.com>
2026-02-28 18:07:04 -0800

    feat: add lazyvim sidekick extra

--- homes/_modules/editor/neovim/nvim/lua/config/lazy.lua
+++ homes/_modules/editor/neovim/nvim/lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.sql" },
+    { import = "lazyvim.plugins.extras.ai.sidekick" },
     -- import/override with your plugins
     { import = "plugins" },
   },
