a0ed1001ff70b1a30fb49188d5ff8c6bc16159a3
Oliver Tosky <42260747+otosky@users.noreply.github.com>
2026-05-20 01:30:34 -0700

    deps: add pspg pager configuration (#108)
    * deps: add pspg pager configuration
    
    * fix: match pspg menu colors

--- homes/_modules/developer/default.nix
+++ homes/_modules/developer/default.nix
@@ -4,8 +4,65 @@
   lib,
   ...
 }: let
+  defaultPspgConfig = pkgs.writeText "pspgconf" ''
+    custom_theme_name = "tokyonight"
+    direct_color = true
+  '';
+
+  defaultPspgTheme = pkgs.writeText "pspg-theme-tokyonight" ''
+    template = 1
+    template_menu = 9
+
+    background = #c0caf5, #1a1b26
+    data = #c0caf5, #1a1b26
+    data* = #c0caf5, #16161e
+    label = #7aa2f7, #1f2335, bold
+    label* = #7aa2f7, #24283b, bold
+    border = #3b4261, #1a1b26
+    border* = #3b4261, #16161e
+    row_number = #565f89, #1a1b26
+    selected_area = #c0caf5, #364a82, bold
+    footer = #a9b1d6, #1f2335
+
+    cursor_data = #1a1b26, #7aa2f7, bold
+    cursor_border = #1a1b26, #7aa2f7, bold
+    cursor_label = #1a1b26, #7aa2f7, bold
+    cursor_row_number = #1a1b26, #7aa2f7, bold
+    cursor_selected_area = #1a1b26, #bb9af7, bold
+    cursor_footer = #1a1b26, #7dcfff, bold
+    cross_cursor = #1a1b26, #bb9af7, bold
+    cross_cursor_border = #1a1b26, #bb9af7, bold
+
+    status_bar = #c0caf5, #1f2335
+    title = #bb9af7, #1f2335, bold
+    prompt_bar = #7dcfff, #1f2335
+    info_bar = #9ece6a, #1f2335
+    input_bar = #c0caf5, #1f2335
+    error_bar = #f7768e, #1f2335, bold
+
+    bookmark = #e0af68, #1a1b26, bold
+    bookmark_border = #e0af68, #1a1b26
+    cursor_bookmark = #1a1b26, #e0af68, bold
+
+    matched_pattern = #1a1b26, #e0af68, bold
+    matched_line = #c0caf5, #292e42
+    matched_line_border = #e0af68, #292e42
+    matched_pattern_cursor = #1a1b26, #ff9e64, bold
+    matched_line_vertical_cursor = #1a1b26, #bb9af7, bold
+    matched_line_vertical_cursor_border = #1a1b26, #bb9af7, bold
+
+    scrollbar_arrows = #7aa2f7, #1f2335
+    scrollbar_background = #3b4261, #1a1b26
+    scrollbar_slider = #7aa2f7, #1a1b26
+    scrollbar_active_slider = #bb9af7, #1a1b26
+  '';
+
   defaultUsqlConfigTemplate = pkgs.writeText "usql-config.yaml.tpl" ''
     # Copy this file per-host and replace the placeholder 1Password references.
+    init: |
+      \set PAGER pspg
+      \pset pager on
+
     connections:
       mysql_example:
         protocol: mysql
@@ -54,6 +111,11 @@ in {
   ];
 
   home = {
+    file = {
+      ".pspgconf".source = defaultPspgConfig;
+      ".pspg_theme_tokyonight".source = defaultPspgTheme;
+    };
+
     activation.installMutableUsqlTemplate = lib.hm.dag.entryAfter ["writeBoundary"] ''
       template_dir="$HOME/.config/usql"
       template_path="$template_dir/config.yaml.tpl"
@@ -73,6 +135,7 @@ in {
         sqlit-tui
         usql
         usqlp
+        pspg
         coursier
         delta
         age
