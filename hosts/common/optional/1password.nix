af751208d5d5e59b30ffc75a86b53afd1617f3b2
Oliver Tosky <42260747+otosky@users.noreply.github.com>
2026-05-30 00:18:17 -0700

    fix: set 1password polkit owners from users (#130)

--- hosts/common/optional/1password.nix
+++ hosts/common/optional/1password.nix
@@ -1,7 +1,11 @@
-{config, ...}: let
-  inherit (config.networking) hostName;
+{
+  config,
+  lib,
+  ...
+}: let
+  normalUsers = lib.attrNames (lib.filterAttrs (_: user: user.isNormalUser or false) config.users.users);
 in {
   programs._1password-gui.enable = true;
   programs._1password.enable = true;
-  programs._1password-gui.polkitPolicyOwners = [hostName];
+  programs._1password-gui.polkitPolicyOwners = normalUsers;
 }
