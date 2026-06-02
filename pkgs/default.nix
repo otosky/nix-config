6f66ace5f4e831278bdfeb2c4ccdcd45075a2d26
Oliver Tosky <olivertosky@gmail.com>
2026-06-01 21:36:24 -0700

    deps: source AI agents from llm-agents.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,5 @@
   usqlp = pkgs.callPackage ./usqlp {};
   agor = pkgs.callPackage ./agor {};
   cymbal = pkgs.callPackage ./cymbal {};
-  pi-coding-agent = pkgs.callPackage ./pi-coding-agent {};
   superdirt = pkgs.callPackage ./superdirt {};
 }
